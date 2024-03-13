83354c2f31bae151005a4779e4fbefd9f7881721
Yury Kavalenka <yurykavalenka@gmail.com>
2024-03-13 15:52:19 +0100

    I finished a index.html page

--- js/main.js
+++ js/main.js
@@ -22,3 +22,63 @@ headers.forEach((item) => {
 function showContent() {
   this.nextElementSibling.classList.toggle("accordeon-body");
 }
+
+const boxes = document.querySelectorAll(".box__tools");
+
+window.addEventListener('scroll', checkBoxes);
+
+
+function checkBoxes() {
+ const triggerBottom = window.innerHeight / 5 * 4;
+
+ boxes.forEach(box => {
+  const boxTop = box.getBoundingClientRect().top;
+
+  if(boxTop < triggerBottom) {
+    box.classList.add('show')
+  } else {
+    box.classList.remove('show')
+  }
+ })
+}
+
+const boxFeatures = document.querySelectorAll(".box-features");
+
+window.addEventListener('scroll', opacityShow);
+
+function opacityShow() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxFeatures.forEach(box => {
+  const boxTop = box.getBoundingClientRect().top;
+
+  if(boxTop < triggerBottom) {
+    box.classList.add('show')
+  } else {
+    box.classList.remove('show')
+  }
+  })
+}
+
+
+// swiper
+
+const mySwiper = new Swiper(".swiper", {
+  spaceBetween: 30,
+  loop: true,
+  centeredSlides: true,
+  //  autoHeight: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  speed: 2000,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
